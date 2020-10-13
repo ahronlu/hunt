d087c273e5360221eb1d33d340f98bc2837ea46e
koftov <ahronlu@gmail.com>
2020-10-13 14:32:40 +0300

    Firebase Authentication

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -4,12 +4,32 @@ import "firebase/firestore";
 
 import firebaseConfig from "./config";
 
-const Firebase {
+class Firebase {
   constructor() {
     app.initializeApp(firebaseConfig);
     this.app = app;
     this.auth = app.auth();
     this.db = app.firestore();
+  }
+
+  async register(name, email, password) {
+    const newUser = await this.auth.createUserWithEmailAndPassword(
+      email,
+      password
+    );
+    return newUser.user.updateProfile({ displayName: name });
+  }
+
+  login(email, password) {
+    return this.auth.signInWithEmailAndPassword(email, password);
+  }
+
+  logout() {
+    return this.auth.signOut();
+  }
+
+  resetPassword(email) {
+    return this.auth.sendPasswordResetEmail(email);
   }
 }
 
